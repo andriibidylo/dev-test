f3ebdf9272f394d1ac15a89ae3c8567aa68d3c61
Andrii Bidylo <andriibidylo@gmail.com>
2024-08-15 10:54:47 -0700

    add Footer component

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+      <footer className="bg-gray-800 text-white text-center p-4">
+        <p>&copy; 2024 Website</p>
+      </footer>
+    );
+  }
+  
+  
