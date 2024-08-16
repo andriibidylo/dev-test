264926392267f01517d38ec2185ce8ed02885a67
Andrii Bidylo <andriibidylo@gmail.com>
2024-08-15 19:24:33 -0700

    add Posts page and DetailedPost page

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { DetailedPostProps, DetailedPostType } from "@/app/types/post";
+import axios from "axios";
+
+export default async function DetailedPost({ params }: DetailedPostProps) {
+  let post: DetailedPostType | null = null;
+
+  if (params.id) {
+    const postId = params.id.match(/^\d+/);
+    if (postId) {
+      const { data } = await axios.get<DetailedPostType>(
+        `${process.env.NEXT_PUBLIC_API_URL}/posts/${postId[0]}?acf_format=standard&_fields=id,title,content,acf`
+      );
+      post = data;
+    }
+  }
+
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  return (
+    <div>
+      <h1>{post.title.rendered}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+    </div>
+  );
+}
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import PostCard from "../components/PostCard";
+import { PostType } from "../types/post";
+
+export default async function Post() {
+  const { data: posts } = await axios.get<PostType[]>(
+    `${process.env.NEXT_PUBLIC_API_URL}/posts?acf_format=standard&orderby=date&_fields=id,title,featured_media,excerpt`
+  );
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="container mx-auto px-4">
+        <h1 className="text-4xl font-bold my-8">All Posts</h1>
+        <ul className="space-y-6">
+          {posts.map((post, index) => (
+            <PostCard key={post.id} index={index} post={post} />
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
+}
