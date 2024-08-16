2c6e4639c2fef7ba687769e71f7913150fe6ecc6
Andrii Bidylo <andriibidylo@gmail.com>
2024-08-15 18:35:13 -0700

    modify Header to display dropdown menu

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -2,14 +2,27 @@
 
 import Link from "next/link";
 import { useRef, useState } from "react";
+import DropdownMenu from "../DropdownMenu";
+import useSWR from "swr";
+import axios from "axios";
+
+const fetcher = async (url: string) => {
+  const { data } = await axios.get(url);
+  return data;
+};
 
 export default function Header() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
 
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const { data: pages, error } = useSWR(
+    "https://dev-test.yourballistic.com/wp-json/wp/v2/pages?acf_format=standard&_fields=id,title",
+    fetcher
+  );
   const toggleDropdown = () => {
     setIsDropdownOpen((prev) => !prev);
   };
+
   return (
     <header className="bg-gray-800 text-white">
       <nav className="container mx-auto p-4 flex justify-between">
@@ -23,9 +36,8 @@ export default function Header() {
           <div
             className="relative"
             ref={dropdownRef}
-            tabIndex={0} // Makes the div focusable
+            tabIndex={0}
             onBlur={(e) => {
-              // Check if the related target is outside the dropdown
               if (!dropdownRef.current?.contains(e.relatedTarget)) {
                 setIsDropdownOpen(false);
               }
@@ -34,23 +46,7 @@ export default function Header() {
             <button onClick={toggleDropdown} className="hover:underline">
               Pages
             </button>
-            {isDropdownOpen && (
-              <div className="absolute bg-white text-black mt-1 p-2 shadow-lg">
-                <Link
-                  href="/page1"
-                  className="block px-4 py-2 hover:bg-gray-200"
-                >
-                  Page 1
-                </Link>
-                <Link
-                  href="/page2"
-                  className="block px-4 py-2 hover:bg-gray-200"
-                >
-                  Page 2
-                </Link>
-                {/* Add more pages dynamically from the API */}
-              </div>
-            )}
+            {isDropdownOpen && <DropdownMenu pages={pages} />}
           </div>
         </div>
       </nav>
