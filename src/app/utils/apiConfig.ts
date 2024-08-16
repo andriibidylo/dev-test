aa905084465c2396937028d19f7f3dac1ee528c8
Andrii Bidylo <andriibidylo@gmail.com>
2024-08-16 08:04:51 -0700

    add api_config

---
+++ src/app/utils/apiConfig.ts
@@ -0,0 +1,6 @@
+export const API_URLS = {
+  PAGES_ALL: `${process.env.NEXT_PUBLIC_API_URL}/pages?acf_format=standard&_fields=id,title`,
+  PAGES_BASE: `${process.env.NEXT_PUBLIC_API_URL}/pages`,
+  MEDIA_BASE: `${process.env.NEXT_PUBLIC_API_URL}/media`,
+  POSTS_BASE: `${process.env.NEXT_PUBLIC_API_URL}/posts`,
+};
